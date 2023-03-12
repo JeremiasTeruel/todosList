8fdde756d1dfa22e51a2c9da79cd4a5645d7fc47
Teje8 <jteruel8@gmail.com>
2023-03-12 01:33:54 -0300

    add a div in the input from input to style it

--- src/todoSearch/TodoSearch.js
+++ src/todoSearch/TodoSearch.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import './TodoSearch.css'
+import { TodoContext } from "../todoContext/TodoContext";
 
-function TodoSearch ({search, setSearch}) {
+function TodoSearch () {
+
+    const {search, setSearch} = useContext(TodoContext);
+
     const onChangeValue = (event) => {
+        // console.log(event.target.value)
         setSearch(event.target.value);
     }
     return(
-        <input 
-        className="TodoSearchInput" 
-        placeholder="Buscar todo's"
-        value={search}
-        onChange={onChangeValue}
-        />
+        <div className="inputContainer">
+            <input className="TodoSearchInput" placeholder="Buscar todo's" value={search} onChange={onChangeValue} />
+        </div>
     )
 }
 
