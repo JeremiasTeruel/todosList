210361a385df40c03ff4b019726e093264a98e52
Teje8 <jteruel8@gmail.com>
2023-03-12 01:32:49 -0300

    add icons from react icons to mark item do it or delete it

--- src/todoItem/TodoItem.js
+++ src/todoItem/TodoItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './TodoItem.css'
+import { AiOutlineCheck } from 'react-icons/ai'
+import { RxCross1 } from 'react-icons/rx'
 
 function TodoItem (props){
     return(
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={props.onComplete}>√</span>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={props.onComplete}><AiOutlineCheck/></span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete" onClick={props.onDelete}>X</span>
+            <span className="Icon Icon-delete" onClick={props.onDelete}><RxCross1/></span>
         </li>
     );
 }
